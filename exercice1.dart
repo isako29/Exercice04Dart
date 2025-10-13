9e40d2f2dc172c4b6df98c73084e12113eb3db1c
Isako29 <corneilekofo003@gmail.com>
2025-10-13 19:51:51 -0400

    creation_du_methode

--- exercice1.dart
+++ exercice1.dart
@@ -1,5 +1,11 @@
 class Couleur(){
   final int r, g, b;
   couleur(this.r , this.g, this.b);
+  Couleur.rouge() : r = 255, g = 0, b = 0;
+  Couleur.vert(): g = 255, r= 0, b =0;
+  Couleur.bleu(): b = 255,r =0, g=0;
+
+  void AfficherCouleur()
+  print("R : $R, v :$g, b :$b");
   
 }
