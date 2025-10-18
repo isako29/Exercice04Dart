cd246e8682eb0f675d0838ac847a15d012b583c5
Isako29 <corneilekofo003@gmail.com>
2025-10-18 19:57:49 -0400

    Ajout des instances film

--- exercice_type_media_04.dart
+++ exercice_type_media_04.dart
@@ -39,3 +39,43 @@ class Film extends Media {
   }
 }
 
+// Classe Livre qui hérite de Media
+class Livre extends Media {
+  final String auteur;
+  
+  // Constructeur appelant le constructeur de la classe mère avec super
+  Livre(super.titre, this.auteur);
+  
+  // Redéfinition de la méthode afficherType
+  @override
+  void afficherType() {
+    print("Ceci est un Livre : $titre par $auteur.");
+  }
+}
+
+// Classe Film qui hérite de Media
+class Film extends Media {
+  final int dureeMinutes;
+  
+  // Constructeur appelant le constructeur de la classe mère avec super
+  Film(super.titre, this.dureeMinutes);
+  
+  // Redéfinition de la méthode afficherType
+  @override
+  void afficherType() {
+    print("Ceci est un Film : $titre (${dureeMinutes}min).");
+  }
+}
+
+void main() {
+  // Création d'une liste de type List<Media>
+  List<Media> catalogue = [];
+  
+  // Ajout d'instances de Livre et Film à la liste
+  catalogue.add(Livre("Le Petit Prince", "Antoine de Saint-Exupéry"));
+  catalogue.add(Film("Inception", 148));
+  catalogue.add(Livre("1984", "George Orwell"));
+  catalogue.add(Film("The Matrix", 136));
+  catalogue.add(Media("Média anonyme")); // Un média générique
+  
+  
