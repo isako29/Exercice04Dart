09b9d3fbb52dd988923d61d657ea94dfc978ef1c
Isako29 <corneilekofo003@gmail.com>
2025-10-18 19:49:08 -0400

    methode void afficherType

---
+++ exercice_type_media_04.dart
@@ -0,0 +1,13 @@
+// Classe mère Media
+class Media {
+  final String titre;
+  
+  // Constructeur simplifié (sugar syntax)
+  Media(this.titre);
+  
+  // Méthode afficherType
+  void afficherType() {
+    print("Ceci est un média générique.");
+  }
+}
+
