9eb5d3261024096b041ac3ba577880353ed550b8
Isako29 <corneilekofo003@gmail.com>
2025-10-18 19:53:24 -0400

    redefinition de la methode afficherType

--- exercice_type_media_04.dart
+++ exercice_type_media_04.dart
@@ -11,3 +11,31 @@ class Media {
   }
 }
 
+// Classe Livre qui hérite de Media
+class Livre extends Media {
+  final String auteur;
+  
+  // Constructeur appelant le constructeur de la classe mère avec super
+  Livre(super.titre, this.auteur);
+  
+  // Redéfinition de la méthode afficherType
+  @override
+  void afficherType() {
+    print("Ceci est un Livre : $titre par $auteur.");
+  }
+}
+
+// Classe Film qui hérite de Media
+class Film extends Media {
+  final int dureeMinutes;
+  
+  // Constructeur appelant le constructeur de la classe mère avec super
+  Film(super.titre, this.dureeMinutes);
+  
+  // Redéfinition de la méthode afficherType
+  @override
+  void afficherType() {
+    print("Ceci est un Film : $titre (${dureeMinutes}min).");
+  }
+}
+
