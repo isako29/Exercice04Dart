d94ee47274e3568e65daac908e7b0612b47e6ab4
Isako29 <corneilekofo003@gmail.com>
2025-10-18 20:08:46 -0400

    ajoui

---
+++ exercice06.dart
@@ -0,0 +1,6 @@
+// Classe Connectable qui agit comme interface
+class Connectable {
+  void connecter(String utilisateur) {} // Méthode sans corps
+  void deconnecter() {} // Méthode sans corps
+}
+
