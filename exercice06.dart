f60f289d775672974e781232722ee41867b70224
Isako29 <corneilekofo003@gmail.com>
2025-11-13 15:52:15 -0500

    exercice_O6_final

--- exercice06.dart
+++ exercice06.dart
@@ -1,10 +1,10 @@
-// Classe Connectable qui agit comme interface
+
 class Connectable {
-  void connecter(String utilisateur) {} // Méthode sans corps
-  void deconnecter() {} // Méthode sans corps
+  void connecter(String utilisateur) {} 
+  void deconnecter() {} 
 }
 
-// Classe ServeurAPI qui implémente l'interface Connectable
+
 class ServeurAPI implements Connectable {
   @override
   void connecter(String utilisateur) {
@@ -17,7 +17,6 @@ class ServeurAPI implements Connectable {
   }
 }
 
-// Classe BaseDeDonnees qui implémente également Connectable
 class BaseDeDonnees implements Connectable {
   @override
   void connecter(String utilisateur) {
@@ -31,21 +30,17 @@ class BaseDeDonnees implements Connectable {
 }
 
 void main() {
-  // Création d'instances de ServeurAPI et BaseDeDonnees
   ServeurAPI serveur = ServeurAPI();
   BaseDeDonnees bdd = BaseDeDonnees();
-  
-  // Création d'une liste de type List<Connectable>
+
   List<Connectable> services = [];
   
-  // Ajout des instances à la liste
   services.add(serveur);
   services.add(bdd);
   
-  // Parcours de la liste et exécution des méthodes
   for (var service in services) {
-    service.connecter("admin"); // Connexion avec l'utilisateur "admin"
-    service.deconnecter();      // Déconnexion
-    print("---"); // Séparateur pour plus de clarté
+    service.connecter("admin"); 
+    service.deconnecter();     
+     
   }
 }
